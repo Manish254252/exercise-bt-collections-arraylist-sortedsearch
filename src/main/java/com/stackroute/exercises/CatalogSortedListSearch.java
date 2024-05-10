a69478a35e8af5d78cb4ff0dbe4b79035c996fd2
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-10 17:44:35 +0530

    Done with ArrayList

--- src/main/java/com/stackroute/exercises/CatalogSortedListSearch.java
+++ src/main/java/com/stackroute/exercises/CatalogSortedListSearch.java
@@ -1,21 +1,48 @@
 package com.stackroute.exercises;
 
 
+
+import java.util.Collections;
 import java.util.List;
 
 public class CatalogSortedListSearch {
-
-
+   List<String> list;
     //write here logic to get sorted Array List which is a global class variable
     public String catalogListSorter(List<String> unSortedCatalogList) {
+        if(unSortedCatalogList == null )
+        {
+            return "The catalog list is null";
+        }
+        if (unSortedCatalogList.isEmpty()) {
+            return "The catalog List is empty";
+        }
+        if(unSortedCatalogList.contains("") || unSortedCatalogList.contains(null)
+        || unSortedCatalogList.contains(" "))
+        {
+            return "The catalog List contains empty or null or blank space as a value";
+        }
+        Collections.sort(unSortedCatalogList);
+        list =unSortedCatalogList;
 
-        return null;
+
+        return unSortedCatalogList.toString();
+
     }
 
     //write here logic to search the input value in sorted Array List
     public String catalogListSearcher(String value) {
-
-        return null;
+        if(value==null || value.isEmpty() || value.equals(" "))
+        {
+            return "Input is not accepted";
+        }
+        for (String str : list)
+        {
+            if(str.equalsIgnoreCase(value))
+            {
+                return "Found: "+str;
+            }
+        }
+        return "Not Found";
     }
 }
 
